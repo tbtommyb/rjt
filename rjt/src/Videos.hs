61a1d4174c5b6ae42131fd7ccbf4a1111bc7c381
Tom Johnson <tom@tmjohnson.co.uk>
2018-10-14 12:03:59 +0100

    Rough implementation of showing videos

---
+++ rjt/src/Videos.hs
@@ -0,0 +1,12 @@
+module Videos where
+
+instagram :: [String]
+instagram = [
+  "BISfT2KDjQp",
+  "BI-Kz2wD1z0",
+  "BNM39AsF9HR",
+  "BL0yY9BlPv3"
+  ]
+
+youtube :: [String]
+youtube = []
---
+++ rjt/src/videos.md
@@ -0,0 +1,1 @@
+I like to have fun while pushing myself in the gym so here's a few videos of what I've been up to lately!
