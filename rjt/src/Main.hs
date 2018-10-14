2af69e2399fcd2d7e20f53cf4c14ec556dac21a8
Tom Johnson <tom@tmjohnson.co.uk>
2018-10-14 22:05:21 +0100

    Extract controller functions

--- rjt/src/Main.hs
+++ rjt/src/Main.hs
@@ -13,20 +13,31 @@ import Web.Scotty hiding (body, header)
 import Text.Blaze.Html.Renderer.Text
 import Control.Monad.IO.Class (liftIO)
 
+renderHomepage :: ActionM ()
+renderHomepage = html $ renderHtml $ Layout.app "RJ Transformations" Home.partial
+
+renderPackages :: ActionM ()
+renderPackages = do
+  packages <- liftIO $ readFile "src/packages.md"
+  html $ renderHtml $ Layout.app "Packages" (Layout.single "Packages" (markdown def (pack packages)))
+
+renderTestimonials :: ActionM ()
+renderTestimonials = html $ renderHtml $ Layout.app "Testimonials" (Testimonials.partial "Testimonials")
+
+renderVideos :: ActionM ()
+renderVideos = do
+  videoContent <- liftIO $ readFile "src/videos.md"
+  html $ renderHtml $ Layout.app "Videos" (Videos.partial "Videos" (markdown def (pack videoContent)))
+
 main :: IO ()
 main = do
   putStrLn "Starting server..."
   scotty 4000 $ do
-    get "/" $ do
-      html $ renderHtml $ Layout.app "RJ Transformations" Home.partial
-    get "/packages" $ do
-      packages <- liftIO $ readFile "src/packages.md"
-      html $ renderHtml $ Layout.app "Packages" (Layout.single "Packages" (markdown def (pack packages)))
-    get "/testimonials" $ do
-      html $ renderHtml $ Layout.app "Testimonials" (Testimonials.partial "Testimonials")
-    get "/videos" $ do
-      videoContent <- liftIO $ readFile "src/videos.md"
-      html $ renderHtml $ Layout.app "Videos" (Videos.partial "Videos" (markdown def (pack videoContent)))
+    get "/" $ renderHomepage
+    get "/index.html" $ renderHomepage
+    get "/packages" $ renderPackages
+    get "/testimonials" $ renderTestimonials
+    get "/videos" $ renderVideos
     -- TODO: maybe replace with middleware
     get "/:dirname/:filename" $ do
       dirname <- param "dirname"
