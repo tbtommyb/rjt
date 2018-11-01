886a755943e19901aafef07f8b0650b5da8c4697
Tom Johnson <tom@tmjohnson.co.uk>
2018-11-01 22:15:09 +0000

    Successfully put DB pool in config

--- rjt/src/Main.hs
+++ rjt/src/Main.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
 import qualified Web.Scotty ()
 import Web.Scotty.Trans as S
 
-import Data.Pool ()
+import Data.Pool (Pool)
 import Database.Persist.Sqlite as DB
 
 import Control.Applicative (Applicative)
-import Control.Monad.Trans.Class (MonadTrans, lift)
+import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Reader (ReaderT, runReaderT)
 import Control.Monad.IO.Class (liftIO, MonadIO)
 import Control.Monad.Reader.Class (MonadReader)
@@ -51,59 +52,53 @@ renderVideos = do
   videoContent <- liftIO $ readFile "src/videos.md"
   html $ renderHtml $ Layout.app "Videos" $ Layout.single "Videos" $ Videos.partial $ markdown def $ T.pack videoContent
 
-data Config = Config { getPool :: ConnectionPool }
+data Config = Config
+  { getPool :: ConnectionPool
+  } deriving (Show)
 
 newtype ConfigM a = ConfigM
-    { runConfigM :: ReaderT Config IO a
-    } deriving (Applicative, Functor, Monad,
-                MonadIO, MonadReader Config)
+  { runConfigM :: ReaderT Config IO a
+  } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Config)
 
--- config <- B.readFile "src/config.json"
--- let cfg = decode config :: Maybe Config.Config
+main :: IO ()
+main = do
+  pool <- runStdoutLoggingT $ createSqlitePool "dev.db" 3
+  setupDb pool
+  let config = Config { getPool = pool }
+  scottyT 4000 (runIO config) =<< runIO config application
 
 runIO :: Config -> ConfigM a -> IO a
 runIO c m = runReaderT (runConfigM m) c
 
-main :: IO ()
-main = do
-    pool <- runStdoutLoggingT $ createSqlitePool "dev.db" 3
-    let cfg = Config { getPool = pool }
-    application <- runIO cfg Main.app
-    scottyT 4000 (runIO cfg) application
+-- DB stuff to be moved out
+insertUsers :: ReaderT SqlBackend IO ()
+insertUsers = do
+  _ <- insert $ User "Tom" "Johnson" "tom@tmjohson.co.uk"
+  _ <- insert $ User "Roland" "Johnson" "roland@rjtransformations.co.uk"
+  return ()
 
--- insertUsers :: ReaderT SqlBackend IO ()
--- insertUsers = do
---   _ <- insert $ User "Tom" $ "Johnson" $ "tom@tmjohson.co.uk"
---   _ <- insert $ User "Roland" $ "Johnson" $ "roland@rjtransformations.co.uk"
-
-runDb :: (MonadTrans t, MonadIO (t ConfigM)) => SqlPersistT IO a -> t ConfigM a
+runDb :: (MonadReader Config m, MonadIO m) => SqlPersistT IO a -> m a
 runDb query = do
-    pool <- lift $ asks getPool
-    liftIO (runSqlPool query pool)
+    pool <- asks getPool
+    liftIO $ runSqlPool query pool
 
-runMigrations :: ReaderT SqlBackend IO ()
-runMigrations = runMigration migrateAll
+setupDb :: Pool SqlBackend -> IO ()
+setupDb pool = runSqlPool (runMigration migrateAll) pool
 
-getUsers :: ActionT T.Text ConfigM ()
-getUsers = do
-    users <- runDb (selectList [] [])
-    html $ T.pack (show (length (users :: [Entity User])))
-
-app :: ConfigM (ScottyT T.Text ConfigM ())
-app = do
-  return $ do
-    runDb runMigrations
-    -- runDb insertUsers
-  return $ do
-    S.get "/hello" $ (html "hello")
-    S.get "/world" $ (html "world")
-    -- S.get "/" $ renderHomepage
-    -- S.get "/index.html" $ renderHomepage
-    -- S.get "/packages" $ renderPackages
-    -- S.get "/testimonials" $ renderTestimonials
-    -- S.get "/videos" $ renderVideos
-    -- -- TODO: maybe replace with middleware
-    -- S.get "/:dirname/:filename" $ do
-    --   dirname <- param "dirname"
-    --   filename <- param "filename"
-    --   file $ "src" </> dirname </> filename
+-- Main application
+application :: ConfigM (ScottyT T.Text ConfigM ())
+application = do
+    return $ do
+      S.get "/users" $ do
+        users <- lift $ runDb (selectList [] [])
+        html $ T.pack $ show $ length(users :: [Entity User])
+      S.get "/" $ renderHomepage
+      S.get "/index.html" $ renderHomepage
+      S.get "/packages" $ renderPackages
+      S.get "/testimonials" $ renderTestimonials
+      S.get "/videos" $ renderVideos
+      -- TODO: maybe replace with middleware
+      S.get "/:dirname/:filename" $ do
+        dirname <- param "dirname"
+        filename <- param "filename"
+        file $ "src" </> dirname </> filename
