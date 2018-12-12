e9fecef793fcad394821e3ea00f60f4366fcc086
Tom Johnson <tom@tmjohnson.co.uk>
2018-12-12 19:30:01 +0000

    Remove unnecessary partial

--- rjt/src/Views/Pages/Home/Home.hs
+++ rjt/src/Views/Pages/Home/Home.hs
@@ -2,11 +2,12 @@
 
 module Views.Pages.Home.Home where
 
-import qualified Views.Partials.Header as Header (partial)
-import qualified Views.Pages.Home.About as About (partial)
-import qualified Views.Pages.Home.Contact as Contact (partial)
+import qualified Data.Text.Lazy as L
 import Text.Hamlet
+import Text.Markdown
 
+import qualified Views.Partials.Header as Header (partial)
+import qualified Views.Pages.Home.Contact as Contact (partial)
 import Content
 
 partial :: Content.Homepage -> Html
